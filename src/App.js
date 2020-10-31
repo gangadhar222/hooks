62ca2bdbe06d05a01c2c2819dbc9d1f6c919ddda
gangadhar222 <gangadhar@helloar.io>
2020-10-31 09:46:51 +0530

    input fields using hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Counter from './components/Counter';
-import UseStateTwo from './components/UseStateTwo'
+import UseStateTwo from './components/UseStateTwo';
+import UseStateThree from './components/UseStateThree';
 
 export default function App() {
   return (
     <div>
       {/* <Counter /> */}
-      <UseStateTwo />
+      {/* <UseStateTwo /> */}
+      <UseStateThree />
     </div>
   )
 }
